a020f3db8694f69da1a1d0503a274d62dfe544be
jmini515 <80841284+jmini515@users.noreply.github.com>
2021-08-12 00:55:30 +0900

    lombok
    @data
    @Builder
    이용하여 test

--- SpringBlog/src/main/java/HelloSpringBlog/SpringBlog/Test/HttpControllerTest.java
+++ SpringBlog/src/main/java/HelloSpringBlog/SpringBlog/Test/HttpControllerTest.java
@@ -1,5 +1,7 @@
 package HelloSpringBlog.SpringBlog.Test;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.hibernate.boot.archive.scan.spi.ScanOptions;
 import org.springframework.web.bind.annotation.*;
 
 //Controller  = 응답(HTML)
@@ -7,6 +9,18 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HttpControllerTest {
+
+    private static final String TAG= "HttpControllerTest";
+    @GetMapping("/http/lombok")
+    public String lopmbokTest(){
+    Member m = Member.builder().username("a").password("123").build();
+
+    System.out.println(TAG + "getter: "+ m.getUsername());
+    m.setUsername("b");
+    System.out.println(TAG + "getter: "+ m.getUsername());
+    return "lombok test ";
+
+    }
 
     //브라우저를 통하여 요청은 get요청밖에 할 수 없다.
     @GetMapping("/http/get")
@@ -26,6 +40,8 @@ public class HttpControllerTest {
     public String deleteTest() {
         return "delete 요청";
     }
+
+
 
 
 }
