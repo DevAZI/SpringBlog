bc1867b983f0517a6db7d0d317fa4da4cde84bb8
jmini515 <80841284+jmini515@users.noreply.github.com>
2021-08-19 01:42:12 +0900

    gloval Exception 화면

--- SpringBlog/src/main/java/HelloSpringBlog/SpringBlog/Handler/GlobalException.java
+++ SpringBlog/src/main/java/HelloSpringBlog/SpringBlog/Handler/GlobalException.java
@@ -1,8 +1,10 @@
 package HelloSpringBlog.SpringBlog.Handler;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+@ControllerAdvice
 @RestController
 public class GlobalException {
 
