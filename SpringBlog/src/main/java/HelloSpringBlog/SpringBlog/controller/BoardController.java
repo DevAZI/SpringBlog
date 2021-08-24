c573ae86b8675c8cb5593dbc5d7131710a447ca7
jmini515 <80841284+jmini515@users.noreply.github.com>
2021-08-25 02:57:50 +0900

    글 페이징

--- SpringBlog/src/main/java/HelloSpringBlog/SpringBlog/controller/BoardController.java
+++ SpringBlog/src/main/java/HelloSpringBlog/SpringBlog/controller/BoardController.java
@@ -1,14 +1,26 @@
 package HelloSpringBlog.SpringBlog.controller;
 
+import HelloSpringBlog.SpringBlog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping({"","/"})
-    public String index() throws Exception {
+    public String index(Model model, @PageableDefault(size =3, sort="id",
+            direction = Sort.Direction.DESC) Pageable pageable) {
+        model.addAttribute("boards", boardService.글목록(pageable));
+
         return "index";
     }
 
--- SpringBlog/src/main/java/HelloSpringBlog/SpringBlog/service/BoardService.java
+++ SpringBlog/src/main/java/HelloSpringBlog/SpringBlog/service/BoardService.java
@@ -6,9 +6,13 @@ import HelloSpringBlog.SpringBlog.model.User;
 import HelloSpringBlog.SpringBlog.repository.BoardRepository;
 import HelloSpringBlog.SpringBlog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 //스프링이 컴포넌트 스캔을 통하여 bean에 등록해줌
 @Service
@@ -26,6 +30,9 @@ public class BoardService {
         boardRepository.save(board);
     }
 
+    public Page<Board> 글목록 (Pageable pageable) {
+        return boardRepository.findAll(pageable);
+    }
     /*@Transactional(readOnly = true ) // select 할때 트랜젝션 시작, 서비스 종료시 트랜잭션 종료 ( 정합성 )
     public User 로그인(User user) {
        return  userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
