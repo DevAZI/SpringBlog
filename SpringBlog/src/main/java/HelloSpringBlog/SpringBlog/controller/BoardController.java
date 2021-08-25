c30b86ba63a932b9ac1fce72589bc1ff1fb8eb9e
jmini515 <80841284+jmini515@users.noreply.github.com>
2021-08-25 22:49:31 +0900

    글쓰기 상세보기 완료

--- SpringBlog/src/main/java/HelloSpringBlog/SpringBlog/controller/BoardController.java
+++ SpringBlog/src/main/java/HelloSpringBlog/SpringBlog/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,6 +23,12 @@ public class BoardController {
         model.addAttribute("boards", boardService.글목록(pageable));
 
         return "index";
+    }
+
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable int id, Model model) {
+        model.addAttribute("board", boardService.글상세보기(id));
+        return "board/detail";
     }
 
     @GetMapping("/board/saveForm")
